ce686a5b4b42d73e4cc2f47d3d093c79ee88f31b
Scott Dixon <dixon.scottr@gmail.com>
2016-11-13 11:13:56 -0500

    Add create, delete for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true,
                      length: {minimum: 6}
   validates :content, presence: true
